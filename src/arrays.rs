3f99df8b84f6c075b04d504e60d0d334b153f385
Vladimir Rurik <vladimir.rurik@gmail.com>
2024-05-12 05:06:54 +0300

    feat: Implement dynamic arrays using different data structures

--- src/arrays.rs
+++ src/arrays.rs
@@ -9,16 +9,33 @@ struct SingleArray<T> {
     data: Vec<T>,
 }
 
+impl<T> SingleArray<T>
+where
+    T: Clone + Default,
+{
+    fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+}
+
 impl<T> DynamicArray<T> for SingleArray<T>
 where
     T: Clone + Default,
 {
     fn add(&mut self, item: T, index: usize) {
-        self.data.insert(index, item);
+        let mut new_data = Vec::with_capacity(self.data.len() + 1);
+        new_data.extend(self.data.iter().cloned().take(index));
+        new_data.push(item);
+        new_data.extend(self.data.iter().cloned().skip(index));
+        self.data = new_data;
     }
 
     fn remove(&mut self, index: usize) -> T {
-        self.data.remove(index)
+        let mut new_data = Vec::with_capacity(self.data.len() - 1);
+        let removed_item = self.data.remove(index);
+        new_data.extend(self.data.iter().cloned());
+        self.data = new_data;
+        removed_item
     }
 }
 
@@ -26,11 +43,23 @@ struct VectorArray<T> {
     data: Vec<T>,
 }
 
+impl<T> VectorArray<T>
+where
+    T: Clone + Default,
+{
+    fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+}
+
 impl<T> DynamicArray<T> for VectorArray<T>
 where
     T: Clone + Default,
 {
     fn add(&mut self, item: T, index: usize) {
+        if self.data.len() == self.data.capacity() {
+            self.data.reserve(self.data.len()); // Double the capacity
+        }
         self.data.insert(index, item);
     }
 
@@ -39,8 +68,22 @@ where
     }
 }
 
+
 struct FactorArray<T> {
     data: Vec<T>,
+    growth_factor: usize,
+}
+
+impl<T> FactorArray<T>
+where
+    T: Clone + Default,
+{
+    fn new(growth_factor: usize) -> Self {
+        Self {
+            data: Vec::with_capacity(growth_factor),
+            growth_factor,
+        }
+    }
 }
 
 impl<T> DynamicArray<T> for FactorArray<T>
@@ -48,6 +91,9 @@ where
     T: Clone + Default,
 {
     fn add(&mut self, item: T, index: usize) {
+        if self.data.capacity() == self.data.len() {
+            self.data.reserve(self.growth_factor * self.data.capacity());
+        }
         self.data.insert(index, item);
     }
 
@@ -57,7 +103,20 @@ where
 }
 
 struct MatrixArray<T> {
-    data: Vec<T>,
+    blocks: Vec<Vec<T>>,
+    block_size: usize,
+}
+
+impl<T> MatrixArray<T>
+where
+    T: Clone + Default,
+{
+    fn new(block_size: usize) -> Self {
+        Self {
+            blocks: Vec::new(),
+            block_size,
+        }
+    }
 }
 
 impl<T> DynamicArray<T> for MatrixArray<T>
@@ -65,14 +124,23 @@ where
     T: Clone + Default,
 {
     fn add(&mut self, item: T, index: usize) {
-        self.data.insert(index, item);
+        // This is a simplified version and doesn't handle all edge cases
+        let block_index = index / self.block_size;
+        let position = index % self.block_size;
+
+        if block_index >= self.blocks.len() {
+            self.blocks.push(Vec::with_capacity(self.block_size));
+        }
+
+        self.blocks[block_index].insert(position, item);
     }
 
     fn remove(&mut self, index: usize) -> T {
-        self.data.remove(index)
+        let block_index = index / self.block_size;
+        let position = index % self.block_size;
+        self.blocks[block_index].remove(position)
     }
 }
-
 struct ArrayList<T> {
     data: Vec<T>,
 }
@@ -111,10 +179,10 @@ where
 }
 
 fn main() {
-    let mut single_array: SingleArray<i32> = SingleArray { data: Vec::new() };
-    let mut vector_array: VectorArray<i32> = VectorArray { data: Vec::new() };
-    let mut factor_array: FactorArray<i32> = FactorArray { data: Vec::new() };
-    let mut matrix_array: MatrixArray<i32> = MatrixArray { data: Vec::new() };
+    let mut single_array: SingleArray<i32> = SingleArray::new();
+    let mut vector_array: VectorArray<i32> = VectorArray::new();
+    let mut factor_array: FactorArray<i32> = FactorArray::new( 2 );
+    let mut matrix_array: MatrixArray<i32> = MatrixArray::new( 10 );
     let mut array_list: ArrayList<i32> = ArrayList { data: Vec::new() };
 
     println!("Testing SingleArray:");
